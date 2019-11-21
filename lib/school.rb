0df525af3e12aff1cc43ff8ea09d5063e3c8b98f
Alex Suthammanont <asuthamm@gmail.com>
2019-11-21 01:18:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :author, :page_count
+  attr_reader :school_name, :student, :grade, :roster
+
+  @@all = []
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+    
+    @@all.push(self)
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def add_student(student, grade)
+    
+    @roster[grade] = [] if !@roster.has_key?(grade)
+    @roster[grade].push(student)
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort 
+    
+    #   @roster.each do |grade|
+    #     grade.sort
+    # end
+    self.sort
+  end
+  
+  
+end
